fdef4ad3c9966640565fc5e87712db5d1677b8b2
JCGoran <goran.jelic-cizmek@unige.ch>
2019-09-18 12:28:50 +0200

    Added comments for covariance den and rsd

--- src/covariance.c
+++ src/covariance.c
@@ -438,9 +438,11 @@ int coffe_covariance_init(
             double matter_bias1 = 0, f = 0;
 
             for (int i = 0; i<par->correlation_sources_len*(par->correlation_sources_len + 1)/2; ++i){
+                /* set bias to nonzero only if there is a density contribution */
                 if (strcmp(par->corr_terms[i], "00") == 0){
                     matter_bias1 = interp_spline(&par->matter_bias1, z_mean);
                 }
+                /* set growth rate to nonzero only if there's an rsd contribution */
                 if (strcmp(par->corr_terms[i], "11") == 0){
                     f = interp_spline(&bg->f, z_mean);
                 }
