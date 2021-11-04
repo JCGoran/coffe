feecaf539f76544cc730ffce38516ee9c26aad47
JCGoran <goran.cizmek@gmail.com>
2021-11-04 21:52:39 +0100

    Forgot to add header

---
+++ src/compatibility.h
@@ -0,0 +1,40 @@
+/* boilerplate for CPP aliasing */
+#ifndef COFFE_COMPATIBILITY
+#define COFFE_COMPATIBILITY
+
+static double creal(complex_t value)
+{
+    return std::real(value);
+}
+
+static double cimag(complex_t value)
+{
+    return std::imag(value);
+}
+
+static complex_t csin(complex_t value)
+{
+    return std::sin(value);
+}
+
+static complex_t ccos(complex_t value)
+{
+    return std::cos(value);
+}
+
+static complex_t cpow(complex_t value, complex_t exponent)
+{
+    return std::pow(value, exponent);
+}
+
+static complex_t cexp(complex_t value)
+{
+    return std::exp(value);
+}
+
+static complex_t clog(complex_t value)
+{
+    return std::log(value);
+}
+
+#endif
