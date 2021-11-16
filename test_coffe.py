448f850bb0862afe6576b17c031b2083dfb66eb8
JCGoran <goran.jelic-cizmek@unige.ch>
2021-11-16 20:28:36 +0100

    Added pytests for corrfunc, multipoles, covariance

--- test_coffe.py
+++ test_coffe.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-from scipy.interpolate import interp1d
+import pandas as pd
 import pytest
 
 import coffe
@@ -32,3 +32,82 @@ class TestCoffe:
                 else:
                     for index, zi in enumerate(d['z']):
                         assert np.isclose(d[key][index], getattr(cosmo, key)(zi))
+
+
+    def test_corrfunc(self):
+        cosmo = coffe.Coffe(
+            # in Mpc/h
+            sep=[10, 20, 40, 100, 150],
+            mu=[0.0, 0.2, 0.5, 0.8, 0.95],
+        )
+
+        k, pk = np.transpose(np.loadtxt('PkL_CLASS.dat'))
+        cosmo.set_power_spectrum_linear(k, pk)
+
+        contributions = {'den' : 'density', 'rsd' : 'rsd', 'len' : 'lensing'}
+        for prefix in contributions:
+            cosmo.reset_contributions()
+            setattr(cosmo, f'has_{contributions[prefix]}', True)
+            result = cosmo.compute_corrfunc_bulk()
+            df = pd.DataFrame([_.to_dict() for _ in result])
+            for index, mu in enumerate(cosmo.mu):
+                data = np.loadtxt(
+                    os.path.join(DATA_DIR, f'benchmark_{prefix}_corrfunc{index}.dat')
+                )
+                x, y = np.transpose(data)
+                assert np.allclose(df.loc[df.mu == mu].r.values, x)
+                assert np.allclose(df.loc[df.mu == mu].value.values, y, rtol=5e-4)
+
+
+    def test_multipoles(self):
+        cosmo = coffe.Coffe(
+            # in Mpc/h
+            sep=[10, 20, 40, 100, 150],
+            l=[0, 2, 4],
+        )
+
+        k, pk = np.transpose(np.loadtxt('PkL_CLASS.dat'))
+        cosmo.set_power_spectrum_linear(k, pk)
+
+        contributions = {'den' : 'density', 'rsd' : 'rsd', 'len' : 'lensing'}
+        for prefix in contributions:
+            cosmo.reset_contributions()
+            setattr(cosmo, f'has_{contributions[prefix]}', True)
+            result = cosmo.compute_multipoles_bulk()
+            df = pd.DataFrame([_.to_dict() for _ in result])
+            for index, mp in enumerate(cosmo.l):
+                data = np.loadtxt(
+                    os.path.join(DATA_DIR, f'benchmark_{prefix}_multipoles{mp}.dat')
+                )
+                x, y = np.transpose(data)
+                assert np.allclose(df.loc[df.l == mp].r.values, x)
+                assert np.allclose(df.loc[df.l == mp].value.values, y, rtol=5e-4)
+
+
+    def test_covariance_multipoles(self):
+        cosmo = coffe.Coffe(
+            has_density=True,
+            has_rsd=True,
+            number_density=[1e-3],
+            z_mean=[1.0],
+            deltaz=[0.1],
+            fsky=[0.2],
+            pixelsize=[50],
+            sep=np.arange(50, 350, 50),
+            l=[0, 2, 4],
+        )
+        k, pk = np.transpose(np.loadtxt('PkL_CLASS.dat'))
+        cosmo.set_power_spectrum_linear(k, pk)
+
+        result = cosmo.compute_covariance_bulk()
+        df = pd.DataFrame([_.to_dict() for _ in result])
+        for index1, mp1 in enumerate(cosmo.l):
+            for index2, mp2 in enumerate(cosmo.l):
+                data = np.loadtxt(
+                    os.path.join(DATA_DIR, f'benchmark_multipoles_covariance_{mp1}{mp2}.dat')
+                )
+                x, y, z = np.transpose(data)
+                assert np.allclose(df.loc[(df.l1 == mp1) & (df.l2 == mp2)].r1.values, x)
+                assert np.allclose(df.loc[(df.l1 == mp1) & (df.l2 == mp2)].r2.values, y)
+                assert np.allclose(df.loc[(df.l1 == mp1) & (df.l2 == mp2)].value.values, z, rtol=5e-4)
+                #assert np.allclose(df.loc[df.mu == mu].value.values, y, rtol=5e-4)
