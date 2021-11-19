95f2767108d394803ed1e1d5fe321aeafc219a49
JCGoran <goran.jelic-cizmek@unige.ch>
2021-11-19 10:29:34 +0100

    Added metadata to setup script

--- setup.py
+++ setup.py
@@ -2,5 +2,11 @@ from setuptools import Extension, setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize([Extension("coffe", ["coffe.pyx"])])
+    name='Coffe',
+    version='3.0',
+    description='The COrrelation Function Full-sky Estimator code',
+    url='https://github.com/JCGoran/coffe',
+    author='Goran Jelic-Cizmek',
+    author_email='goran.jelic-cizmek@unige.ch',
+    ext_modules=cythonize([Extension("coffe", ["coffe.pyx"])])
 )
