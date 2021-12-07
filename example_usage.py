21656cc061bff1c9137bb45bd8970ff80630dc95
JCGoran <goran.jelic-cizmek@unige.ch>
2021-12-07 12:56:48 +0100

    Removed old Python wrapper and updated example

--- example_usage.py
+++ example_usage.py
@@ -1,27 +1,22 @@
 # Example usage of the Python wrapper
-# note that the COFFE binary must be already built
-# using `make`
-# to run this script itself, use `python example_usage.py`
-import sys
-
-# this is needed to correctly import COFFE (from the current directory)
-sys.path.append('python/')
-
+import numpy as np
 import coffe
 
-# running with some parameters
-coffe.run_coffe(
-    parameters={
-        'z_mean' : 1.5,
-        'correlation_contributions' : ['den', 'rsd'],
+
+
+# setting the parameters
+cosmology = coffe.Coffe(
+    **{
+        'z_mean' : [1.5],
+        'has_density' : True,
+        'has_rsd' : True,
         'mu' : [0.0, 0.1, 0.5, 0.7, 0.9, 0.95, 0.98, 0.99],
         'omega_baryon' : 0.04,
         'omega_cdm' : 0.26,
-        'output_path' : 'python_wrapper_test/',
-        'output_prefix' : '$TIME',
-        'output_type' : 1,
-        'input_separations' : 'separations.dat',
+        'sep' : np.loadtxt('separations.dat'),
     },
-    cores=1,
-    path='./coffe',
 )
+
+# computes the 2PCF for the above parameters
+result = cosmology.compute_corrfunc_bulk()
+print(result)
