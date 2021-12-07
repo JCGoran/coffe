9ca48ca30faa815e8482fde4061a8091210ee416
JCGoran <goran.jelic-cizmek@unige.ch>
2021-12-07 12:52:57 +0100

    Added install_requires to setup

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
     url='https://github.com/JCGoran/coffe',
     author='Goran Jelic-Cizmek',
     author_email='goran.jelic-cizmek@unige.ch',
+    install_requires=['numpy>=1.19.5'],
     ext_modules=cythonize([Extension("coffe", ["coffe.pyx"])])
 )
