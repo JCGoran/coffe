2da5a1c8c0bde56592f3f4784f85cb97739945ea
JCGoran <goran.jelic-cizmek@unige.ch>
2023-01-20 12:06:10 +0100

    Added installation script for COFFE

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# THIS IS THE ONLY SCRIPT YOU NEED TO RUN TO INSTALL COFFE
+
+set -euxo
+
+# in case of no conda env, we just install the Python-specific requirements
+if [ -z "${CONDA_PREFIX-}" ] && [ -z "${CONDA_DEFAULT_ENV-}" ]
+then
+    python3 -m pip install Cython wheel
+# otherwise, we need to install all of the requirements (compiler, GSL lib, etc.)
+else
+    conda install --channel conda-forge --file requirements.txt
+fi
+
+git submodule update --init --recursive
+git submodule foreach './install.sh'
+python3 setup.py build_ext -i
+python3 -m pip install .
+
+set +euxo
