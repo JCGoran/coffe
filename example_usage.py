6c39ae51581d92bdc351efd23d9a9f94613cc208
JCGoran <goran.jelic-cizmek@unige.ch>
2023-02-10 13:15:11 +0100

    Updated example usage file

--- example_usage.py
+++ example_usage.py
@@ -1,22 +1,26 @@
+#!/usr/bin/env python3
+
 # Example usage of the Python wrapper
+
 import numpy as np
+
 import coffe
 
+if __name__ == "__main__":
+    # setting the parameters
+    cosmology = coffe.Coffe(
+        **{
+            "z_mean": [1.5],
+            "deltaz": [0.1],
+            "has_density": True,
+            "has_rsd": True,
+            "mu": [0.0, 0.1, 0.5, 0.7, 0.9, 0.95, 0.98, 0.99],
+            "omega_baryon": 0.04,
+            "omega_m": 0.31,
+            "sep": np.arange(5, 100, 5),
+        },
+    )
 
-
-# setting the parameters
-cosmology = coffe.Coffe(
-    **{
-        'z_mean' : [1.5],
-        'has_density' : True,
-        'has_rsd' : True,
-        'mu' : [0.0, 0.1, 0.5, 0.7, 0.9, 0.95, 0.98, 0.99],
-        'omega_baryon' : 0.04,
-        'omega_cdm' : 0.26,
-        'sep' : np.loadtxt('separations.dat'),
-    },
-)
-
-# computes the 2PCF for the above parameters
-result = cosmology.compute_corrfunc_bulk()
-print(result)
+    # computes the 2PCF for the above parameters
+    result = cosmology.compute_corrfunc_bulk()
+    print(result)
