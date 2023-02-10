3d1ec8c32314b29e26b4c582d843e66689a1d34f
JCGoran <goran.jelic-cizmek@unige.ch>
2023-02-10 14:08:40 +0100

    Updated script for generating docs

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
 
-python3 -m pdoc --docformat numpy --math -o docs/ coffe
+set -eux
+
+python3 -m pdoc --docformat numpy --math -o docs/ ./coffe
+
+printf "Docs generated at: %s\n" "${PWD}/docs/index.html"
+
+set +eux
