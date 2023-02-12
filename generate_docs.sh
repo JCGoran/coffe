f5487ff429eedaf6be1887c40f0ebe2b3f4c001d
JCGoran <goran.jelic-cizmek@unige.ch>
2023-02-12 14:26:59 +0100

    Added dev requirements to GH action

--- generate_docs.sh
+++ generate_docs.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-python3 -m pdoc --docformat numpy --math -o docs/ ./coffe
+python3 -m pdoc --docformat numpy --math -o docs/ coffe
 
 printf "Docs generated at: %s\n" "${PWD}/docs/index.html"
 
