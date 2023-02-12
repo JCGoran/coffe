9bde84370ca4133f08aa4cbed80212dfa80e580f
JCGoran <goran.jelic-cizmek@unige.ch>
2023-02-12 13:05:15 +0100

    Updated __init__.py docstring

--- coffe/__init__.py
+++ coffe/__init__.py
@@ -1,7 +1,7 @@
 r"""
 ## COFFE - COrrelation Function Full-sky Estimator code
 
-This is the Cython (Python) version of the COFFE code.
+This is the Cython (Python) version of the COFFE code, version 3.
 
 ### Installation
 
@@ -11,14 +11,33 @@ In brief, if you are on Linux, you can install COFFE using:
 pip install coffe
 ```
 
-Refer to the [installation
-section](https://github.com/JCGoran/coffe/#installation) of the README for
-installation instructions on various other platforms.
+Refer to the installation section of the
+[README](https://github.com/JCGoran/coffe/) for installation instructions on
+various other platforms.
 
 ### Running
 
-For various examples of the capabilities of COFFE, have a look at the
+#### From a Python environment
+
+For various examples of using COFFE in a Python environment, have a look at the
 `coffe.coffe` submodule.
+
+#### From the command line
+
+COFFE also has a command line interface (CLI), which can be invoked after
+installation as:
+
+```sh
+coffe-cli -s [SETTINGS_FILE] -o [OUTPUT_FILE] -k [TYPE_OF_OUTPUT]
+```
+
+where `[SETTINGS_FILE]` is the path to the settings file (for a list of
+available settings, as well as defaults, have a look at the [default settings
+file](https://github.com/JCGoran/coffe/blob/master/DEFAULT_SETTINGS.cfg)),
+`[OUTPUT_FILE]` is the file where the output will be placed, and
+`[TYPE_OF_OUTPUT]` is one of `background`, `corrfunc`, `multipoles`, and
+`covariance`, for outputs of the background quantities, the 2-point correlation
+function, its multipoles, and the covariance of its multipoles, respectively.
 """
 
 from coffe.coffe import Coffe
