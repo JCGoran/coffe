9cc4a2cad347e70462bbab70b019fdee9e1c8560
Goran Jelic-Cizmek <jcgoran@protonmail.com>
2024-07-28 21:34:32 +0200

    Format cmake files with cmake-format

--- cmake/FindCython.cmake
+++ cmake/FindCython.cmake
@@ -1,22 +1,18 @@
-#.rst:
+# .rst:
 #
 # Find ``cython`` executable.
 #
 # This module will set the following variables in your project:
 #
-#  ``CYTHON_EXECUTABLE``
-#    path to the ``cython`` program
+# ``CYTHON_EXECUTABLE`` path to the ``cython`` program
 #
-#  ``CYTHON_VERSION``
-#    version of ``cython``
+# ``CYTHON_VERSION`` version of ``cython``
 #
-#  ``CYTHON_FOUND``
-#    true if the program was found
+# ``CYTHON_FOUND`` true if the program was found
 #
 # And the following target:
 #
-#  ``Cython::Cython``
-#    The Cython executable
+# ``Cython::Cython`` The Cython executable
 #
 # A range of versions is supported on CMake 3.19+. See also UseCython.
 #
@@ -25,24 +21,24 @@
 # *Cython is a language that makes writing C extensions for the Python language
 # as easy as Python itself.*
 #
-#=============================================================================
+# =============================================================================
 # Copyright 2011 Kitware, Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#=============================================================================
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+# =============================================================================
 
-# Use the Cython executable that lives next to the Python executable
-# if it is a local installation.
+# Use the Cython executable that lives next to the Python executable if it is a
+# local installation.
 if(Python_EXECUTABLE)
   get_filename_component(_python_path ${Python_EXECUTABLE} PATH)
 elseif(Python3_EXECUTABLE)
@@ -52,25 +48,27 @@ elseif(DEFINED PYTHON_EXECUTABLE)
 endif()
 
 if(DEFINED _python_path)
-  find_program(CYTHON_EXECUTABLE
-               NAMES cython cython.bat cython3
-               HINTS ${_python_path}
-               DOC "path to the cython executable")
+  find_program(
+    CYTHON_EXECUTABLE
+    NAMES cython cython.bat cython3
+    HINTS ${_python_path}
+    DOC "path to the cython executable")
 else()
-  find_program(CYTHON_EXECUTABLE
-               NAMES cython cython.bat cython3
-               DOC "path to the cython executable")
+  find_program(
+    CYTHON_EXECUTABLE
+    NAMES cython cython.bat cython3
+    DOC "path to the cython executable")
 endif()
 
 if(CYTHON_EXECUTABLE)
   set(CYTHON_version_command "${CYTHON_EXECUTABLE}" --version)
 
-  execute_process(COMMAND ${CYTHON_version_command}
-                  OUTPUT_VARIABLE CYTHON_version_output
-                  ERROR_VARIABLE CYTHON_version_error
-                  RESULT_VARIABLE CYTHON_version_result
-                  OUTPUT_STRIP_TRAILING_WHITESPACE
-                  ERROR_STRIP_TRAILING_WHITESPACE)
+  execute_process(
+    COMMAND ${CYTHON_version_command}
+    OUTPUT_VARIABLE CYTHON_version_output
+    ERROR_VARIABLE CYTHON_version_error
+    RESULT_VARIABLE CYTHON_version_result
+    OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_STRIP_TRAILING_WHITESPACE)
 
   if(NOT ${CYTHON_version_result} EQUAL 0)
     set(_error_msg "Command \"${CYTHON_version_command}\" failed with")
@@ -93,18 +91,16 @@ else()
   set(_handle_version_range HANDLE_VERSION_RANGE)
 endif()
 
-find_package_handle_standard_args(Cython
+find_package_handle_standard_args(
+  Cython
   REQUIRED_VARS CYTHON_EXECUTABLE
-  VERSION_VAR ${CYTHON_VERSION}
-  ${_handle_version_range}
-)
+  VERSION_VAR ${CYTHON_VERSION} ${_handle_version_range})
 
 if(CYTHON_FOUND)
   if(NOT DEFINED Cython::Cython)
     add_executable(Cython::Cython IMPORTED)
-    set_target_properties(Cython::Cython PROPERTIES
-        IMPORTED_LOCATION "${CYTHON_EXECUTABLE}"
-    )
+    set_target_properties(Cython::Cython PROPERTIES IMPORTED_LOCATION
+                                                    "${CYTHON_EXECUTABLE}")
   endif()
 endif()
 
--- cmake/UseCython.cmake
+++ cmake/UseCython.cmake
@@ -1,4 +1,4 @@
-#.rst:
+# .rst:
 #
 # The following functions are defined:
 #
@@ -7,86 +7,68 @@
 # Create custom rules to generate the source code for a Python extension module
 # using cython.
 #
-#   Cython_transpile(<pyx_file>
-#                   [LANGUAGE C | CXX]
-#                   [CYTHON_ARGS <args> ...]
-#                   [OUTPUT <OutputFile>]
-#                   [OUTPUT_VARIABLE <OutputVariable>])
+# Cython_transpile(<pyx_file> [LANGUAGE C | CXX] [CYTHON_ARGS <args> ...]
+# [OUTPUT <OutputFile>] [OUTPUT_VARIABLE <OutputVariable>])
 #
 # Options:
 #
-# ``LANGUAGE [C | CXX]``
-#   Force the generation of either a C or C++ file. Recommended; will attempt
-#   to be deduced if not specified, defaults to C unless only CXX is enabled.
+# ``LANGUAGE [C | CXX]`` Force the generation of either a C or C++ file.
+# Recommended; will attempt to be deduced if not specified, defaults to C unless
+# only CXX is enabled.
 #
-# ``CYTHON_ARGS <args>``
-#   Specify additional arguments for the cythonization process. Will default to
-#   the ``CYTHON_ARGS`` variable if not specified.
+# ``CYTHON_ARGS <args>`` Specify additional arguments for the cythonization
+# process. Will default to the ``CYTHON_ARGS`` variable if not specified.
 #
-# ``OUTPUT <OutputFile>``
-#   Specify a specific path for the output file as ``<OutputFile>``. By
-#   default, this will output into the current binary dir. A depfile will be
-#   created alongside this file as well.
+# ``OUTPUT <OutputFile>`` Specify a specific path for the output file as
+# ``<OutputFile>``. By default, this will output into the current binary dir. A
+# depfile will be created alongside this file as well.
 #
-# ``OUTPUT_VARIABLE <OutputVariable>``
-#   Set the variable ``<OutputVariable>`` in the parent scope to the path to the
-#   generated source file.
+# ``OUTPUT_VARIABLE <OutputVariable>`` Set the variable ``<OutputVariable>`` in
+# the parent scope to the path to the generated source file.
 #
 # Defined variables:
 #
 # ``<OutputVariable>``
-#   The path of the generated source file.
+# The path of the generated source file.
 #
 # Usage example:
 #
 # .. code-block:: cmake
 #
-#   find_package(Cython)
-#   include(UseCython)
+# find_package(Cython) include(UseCython)
 #
-#   Cython_transpile(_hello.pyx
-#     OUTPUT_VARIABLE _hello_source_files
-#   )
+# Cython_transpile(_hello.pyx OUTPUT_VARIABLE _hello_source_files )
 #
-#   Python_add_library(_hello
-#     MODULE ${_hello_source_files}
-#     WITH_SOABI
-#   )
+# Python_add_library(_hello MODULE ${_hello_source_files} WITH_SOABI )
 #
-#
-#=============================================================================
+# =============================================================================
 # Copyright 2011 Kitware, Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#=============================================================================
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+# =============================================================================
 
 if(CMAKE_VERSION VERSION_LESS "3.8")
-  # CMake 3.7 required for DEPFILE
-  # CMake 3.8 required for COMMAND_EXPAND_LISTS
+  # CMake 3.7 required for DEPFILE CMake 3.8 required for COMMAND_EXPAND_LISTS
   message(FATAL_ERROR "CMake 3.8 required for COMMAND_EXPAND_LISTS")
 endif()
 
 function(Cython_transpile)
-  set(_options )
+  set(_options)
   set(_one_value LANGUAGE OUTPUT OUTPUT_VARIABLE)
   set(_multi_value CYTHON_ARGS)
 
-  cmake_parse_arguments(_args
-    "${_options}"
-    "${_one_value}"
-    "${_multi_value}"
-    ${ARGN}
-    )
+  cmake_parse_arguments(_args "${_options}" "${_one_value}" "${_multi_value}"
+                        ${ARGN})
 
   if(DEFINED CYTHON_EXECUTABLE)
     set(_cython_command "${CYTHON_EXECUTABLE}")
@@ -107,7 +89,9 @@ function(Cython_transpile)
   set(_source_files ${_args_UNPARSED_ARGUMENTS})
   list(LENGTH _source_files input_length)
   if(NOT input_length EQUAL 1)
-    message(FATAL_ERROR "One and only one input file must be specified, got '${_source_files}'")
+    message(
+      FATAL_ERROR
+        "One and only one input file must be specified, got '${_source_files}'")
   endif()
 
   function(_transpile _source_file generated_file language)
@@ -122,8 +106,8 @@ function(Cython_transpile)
 
     set_source_files_properties(${generated_file} PROPERTIES GENERATED TRUE)
 
-    # Generated depfile is expected to have the ".dep" extension and be located along
-    # side the generated source file.
+    # Generated depfile is expected to have the ".dep" extension and be located
+    # along side the generated source file.
     set(_depfile ${generated_file}.dep)
     set(_depfile_arg "-M")
 
@@ -131,11 +115,12 @@ function(Cython_transpile)
     get_filename_component(_source_file "${_source_file}" ABSOLUTE)
 
     # Pretty-printed output names
-    file(RELATIVE_PATH generated_file_relative
-        ${CMAKE_BINARY_DIR} ${generated_file})
-    file(RELATIVE_PATH source_file_relative
-        ${CMAKE_SOURCE_DIR} ${_source_file})
-    set(comment "Generating ${_language} source '${generated_file_relative}' from '${source_file_relative}'")
+    file(RELATIVE_PATH generated_file_relative ${CMAKE_BINARY_DIR}
+         ${generated_file})
+    file(RELATIVE_PATH source_file_relative ${CMAKE_SOURCE_DIR} ${_source_file})
+    set(comment
+        "Generating ${_language} source '${generated_file_relative}' from '${source_file_relative}'"
+    )
 
     # Get output directory to ensure its exists
     get_filename_component(output_directory "${generated_file}" DIRECTORY)
@@ -145,23 +130,14 @@ function(Cython_transpile)
     # Add the command to run the compiler.
     add_custom_command(
       OUTPUT ${generated_file}
-      COMMAND
-        ${CMAKE_COMMAND} -E make_directory ${output_directory}
-      COMMAND
-        ${_cython_command}
-        ${_language_arg}
-        "${_args_CYTHON_ARGS}"
-        ${_depfile_arg}
-        ${pyx_location}
-        --output-file ${generated_file}
+      COMMAND ${CMAKE_COMMAND} -E make_directory ${output_directory}
+      COMMAND ${_cython_command} ${_language_arg} "${_args_CYTHON_ARGS}"
+              ${_depfile_arg} ${pyx_location} --output-file ${generated_file}
       COMMAND_EXPAND_LISTS
-      MAIN_DEPENDENCY
-        ${_source_file}
-      DEPFILE
-        ${_depfile}
+      MAIN_DEPENDENCY ${_source_file}
+      DEPFILE ${_depfile}
       VERBATIM
-      COMMENT ${comment}
-    )
+      COMMENT ${comment})
   endfunction()
 
   function(_set_output _input_file _language _output_var)
@@ -173,12 +149,13 @@ function(Cython_transpile)
       message(FATAL_ERROR "_set_output language must be one of C or CXX")
     endif()
 
-    # Can use cmake_path for CMake 3.20+
-    # cmake_path(GET _input_file STEM basename)
+    # Can use cmake_path for CMake 3.20+ cmake_path(GET _input_file STEM
+    # basename)
     get_filename_component(_basename "${_input_file}" NAME_WE)
 
     if(IS_ABSOLUTE ${_input_file})
-      file(RELATIVE_PATH _input_relative ${CMAKE_CURRENT_SOURCE_DIR} ${_input_file})
+      file(RELATIVE_PATH _input_relative ${CMAKE_CURRENT_SOURCE_DIR}
+           ${_input_file})
     else()
       set(_input_relative ${_input_file})
     endif()
@@ -189,7 +166,9 @@ function(Cython_transpile)
       set(_output_relative_dir "${_output_relative_dir}/")
     endif()
 
-    set(${_output_var} "${CMAKE_CURRENT_BINARY_DIR}/${_output_relative_dir}${_basename}.${_language_extension}" PARENT_SCOPE)
+    set(${_output_var}
+        "${CMAKE_CURRENT_BINARY_DIR}/${_output_relative_dir}${_basename}.${_language_extension}"
+        PARENT_SCOPE)
   endfunction()
 
   set(generated_files)
@@ -210,7 +189,9 @@ function(Cython_transpile)
       # Likewise for CXX
       set(_language "CXX")
     else()
-      message(FATAL_ERROR "LANGUAGE keyword required if neither C nor CXX enabled globally")
+      message(
+        FATAL_ERROR
+          "LANGUAGE keyword required if neither C nor CXX enabled globally")
     endif()
   endif()
 
@@ -232,18 +213,24 @@ function(Cython_transpile)
   # Output variable only if set
   if(_args_OUTPUT_VARIABLE)
     set(_output_variable ${_args_OUTPUT_VARIABLE})
-    set(${_output_variable} ${generated_files} PARENT_SCOPE)
+    set(${_output_variable}
+        ${generated_files}
+        PARENT_SCOPE)
   endif()
 
 endfunction()
 
 function(_cython_compute_language OUTPUT_VARIABLE FILENAME)
   file(READ "${FILENAME}" FILE_CONTENT)
-  # Check for compiler directive similar to "# distutils: language = c++"
-  # See https://cython.readthedocs.io/en/latest/src/userguide/wrapping_CPlusPlus.html#declare-a-var-with-the-wrapped-c-class
-  set(REGEX_PATTERN [=[^[[:space:]]*#[[:space:]]*distutils:.*language[[:space:]]*=[[:space:]]*(c\\+\\+|c)]=])
+  # Check for compiler directive similar to "# distutils: language = c++" See
+  # https://cython.readthedocs.io/en/latest/src/userguide/wrapping_CPlusPlus.html#declare-a-var-with-the-wrapped-c-class
+  set(REGEX_PATTERN
+      [=[^[[:space:]]*#[[:space:]]*distutils:.*language[[:space:]]*=[[:space:]]*(c\\+\\+|c)]=]
+  )
   string(REGEX MATCH "${REGEX_PATTERN}" MATCH_RESULT "${FILE_CONTENT}")
   string(TOUPPER "${MATCH_RESULT}" LANGUAGE_NAME)
   string(REPLACE "+" "X" LANGUAGE_NAME "${LANGUAGE_NAME}")
-  set(${OUTPUT_VARIABLE} ${LANGUAGE_NAME} PARENT_SCOPE)
+  set(${OUTPUT_VARIABLE}
+      ${LANGUAGE_NAME}
+      PARENT_SCOPE)
 endfunction()
